87f50abfad94a9200eb925ae52bed3e717153e01
Dominic Faulring <dfaulring@gmail.com>
2022-04-06 19:32:48 -0700

    form structure

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -7,44 +7,88 @@ const StyledForm = styled.form`
   flex-direction: column;
 `;
 
+const StyledFormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  input {
+    height: 48px;
+    padding-left: 8px;
+  }
+
+  textarea {
+    resize: none;
+    height: 160px;
+  }
+
+  .error-message {
+    color: red;
+  }
+`;
+
+const StyledButton = styled.button`
+  height: 48px;
+`;
+
 export default function ContactForm() {
+  const [loading, setLoading] = useState(false);
   const {
     register,
-    handleSubmit
-    // formState: { errors }
+    handleSubmit,
+    formState: { errors }
   } = useForm();
 
   const onSubmit = async (data) => {
     try {
-      const res = await fetch('https://formspree.io/f/moqkgpoz', {
+      setLoading(true);
+      await fetch('https://formspree.io/f/moqkgpoz', {
         method: 'POST',
         headers: {
           Accept: 'application/json'
         },
         body: JSON.stringify(data)
       });
-      console.log(await res.json())
-
+      setLoading(false);
     } catch (err) {
       console.log(err);
+      setLoading(false);
     }
   };
 
   return (
     <StyledForm onSubmit={handleSubmit(onSubmit)}>
-      <div>
+      <StyledFormGroup>
+        <label htmlFor='name'>Name</label>
+        <input
+          type='name'
+          name='name'
+          {...register('name', { required: true })}
+        />
+        {errors.name && (
+          <span className='error-message'>This field is required</span>
+        )}
+      </StyledFormGroup>
+      <StyledFormGroup>
         <label htmlFor='email'>Email Address</label>
         <input
           type='email'
           name='email'
           {...register('email', { required: true })}
         />
-      </div>
-      <div>
+        {errors.name && (
+          <span className='error-message'>This field is required</span>
+        )}
+      </StyledFormGroup>
+      <StyledFormGroup>
         <label htmlFor='message'>Message</label>
         <textarea name='message' {...register('message', { required: true })} />
-      </div>
-      <button type='submit'>Submit</button>
+        {errors.name && (
+          <span className='error-message'>This field is required</span>
+        )}
+      </StyledFormGroup>
+      <StyledButton type='submit' disabled={loading}>
+        Submit
+      </StyledButton>
     </StyledForm>
   );
 }
